22c6101aa66cf829fed64110119f5200e03ce58a
devHksingh <hksingh.dev@gmail.com>
2023-12-06 21:23:04 +0530

    2nd question done

--- assignments/js/week1_0-1_js/easy/expenditure-analysis.js
+++ assignments/js/week1_0-1_js/easy/expenditure-analysis.js
@@ -7,79 +7,36 @@
 
 function calculateTotalSpentByCategory(transactions) {
   let result =[]
+  let totalCategory = new Map()
   transactions.map((item)=>{
-    let totalCategory = new Map()
-    if(!totalCategory.get(item['category'])){
-      totalCategory.set(category,item['category'])
-      totalCategory.set(totalSpent,item['price'])
+    
+    const category = item['category']
+    
+    if(!totalCategory.has(category)){
       
-    }else if(totalCategory.has(item['category'])){
-      totalCategory.set(totalSpent,totalCategory.get(totalSpent)+item['price'])
+      totalCategory.set(category,{'category': category , 'totalSpent':item['price']})
+      
+      
+    }else {
+      
+      const updatePrice = totalCategory.get(category)['totalSpent'] +item['price']
+
+      totalCategory.set(category,{'category':category,'totalSpent':updatePrice})
     }
+  
   })
 
 
-  return [];
 
-}
 
-// module.exports = calculateTotalSpentByCategory;
-
-const transactions = [
-  {
-    id: 1,
-    timestamp: 1656076800000,
-    price: 10,
-    category: 'Food',
-    itemName: 'Pizza',
-  },
-  {
-    id: 2,
-    timestamp: 1656259600000,
-    price: 20,
-    category: 'Food',
-    itemName: 'Burger',
-  },
-  {
-    id: 3,
-    timestamp: 1656019200000,
-    price: 15,
-    category: 'Clothing',
-    itemName: 'T-Shirt',
-  },
-  {
-    id: 4,
-    timestamp: 1656364800000,
-    price: 30,
-    category: 'Electronics',
-    itemName: 'Headphones',
-  },
-  {
-    id: 5,
-    timestamp: 1656105600000,
-    price: 25,
-    category: 'Clothing',
-    itemName: 'Jeans',
-  },
-];
-transactions.map((item)=>{
   
-  console.log(item["category"]);
-})
+  totalCategory.forEach((item)=>{
+    
+    result.push(item)
+  })
+  return result;
 
-// console.log(transactions.[0].);
-let k = new Map();
-
-k.set('1', 'str1');   // a string key
-k.set(1, 'num1');     // a numeric key
-k.set(true, 'bool1');
-
-// console.log(k.get(1));
-if(!k.get(4)){
-  k.set(4,"four")
-  console.log(k);
 }
-if(k.has(4)){
-  k.set(4,"FOUR")
-}
-console.log(k);
+
+module.exports = calculateTotalSpentByCategory;
+
