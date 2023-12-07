1e5c83f7daad2b05e14ecbe6f475fd4599b75921
devHksingh <hksingh.dev@gmail.com>
2023-12-07 23:31:08 +0530

    palidrome done

--- assignments/js/week1_0-1_js/medium/palindrome.js
+++ assignments/js/week1_0-1_js/medium/palindrome.js
@@ -4,7 +4,35 @@
 */
 
 function isPalindrome(str) {
+  str = str.toLowerCase()
+  str =str.split('')
+  let toCheckPalidrome =[]
+  const alfabet = "abcdefghijklmnopqrstuvwxyz"
+  const validVariable = alfabet.split('')
+  if (str.length ===1){
+    return true;
+  }else if(str.length ===1){
+    return true;
+  }
+  
+  for(let i=0;i<str.length;i++){
+    if(validVariable.includes(str[i])){
+      toCheckPalidrome.push(str[i])
+    }
+  }
+  const arrayLength = toCheckPalidrome.length
+  // console.log(Math.floor(toCheckPalidrome.length/2));
+  // console.log(toCheckPalidrome);
+
+  for(let i=0;i<Math.floor(toCheckPalidrome.length/2);i++){
+    if(!(toCheckPalidrome[i]===toCheckPalidrome[arrayLength-1-i])){
+      return false
+    }
+  }
+
   return true;
 }
 
 module.exports = isPalindrome;
+// let test = "race car"
+// console.log(isPalindrome(test));
