ff03c31ba721c6633bc56dedc06fb65ad35e92c2
devHksingh <hksingh.dev@gmail.com>
2023-12-08 12:57:11 +0530

    count vowels question done

--- assignments/js/week1_0-1_js/medium/countVowels.js
+++ assignments/js/week1_0-1_js/medium/countVowels.js
@@ -7,6 +7,21 @@
 
 function countVowels(str) {
     // Your code here
+    const vowels =['a', 'e', 'i', 'o', 'u']
+    const alfabet = "abcdefghijklmnopqrstuvwxyz".split('')
+    str = str.toLowerCase()
+    str = str.split('')
+    let count =0
+    let validStr =[]
+    if ((str.length === 0) ) return 0
+    str.map((word)=>{
+      if((alfabet.includes(word))&&(vowels.includes(word))){
+        count+=1
+      }
+    })
+    return count
+
+
 }
 
 module.exports = countVowels;
