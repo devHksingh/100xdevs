57ef64d5721dd3d8584c6c41aecf1ccdd078771b
devHksingh <hksingh.dev@gmail.com>
2023-12-19 11:19:03 +0530

    add questions and solve 1 promise task

--- assignments/js/week-2/01-async-js/hard (promises)/1-promisify-setTimeout.js
+++ assignments/js/week-2/01-async-js/hard (promises)/1-promisify-setTimeout.js
@@ -3,4 +3,25 @@
 */
 
 function wait(n) {
+    return new Promise((resolve,reject)=>{
+        setTimeout(() => {
+            // console.log('inside wait');
+            resolve();
+        }, n*1000);
+    })
+    
 }
+
+async function callingPromise(){
+    try {
+        const data = await wait(2) 
+        console.log(data);
+        
+    } catch (error) {
+        console.error(error)
+    }
+    
+}
+// callingPromise()
+
+module.exports=wait
