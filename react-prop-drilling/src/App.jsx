3981f4b0c1978665471bcb1b9b7b50ff879c707c
devHksingh <hksingh.dev@gmail.com>
2024-01-15 18:06:42 +0530

    learn contex api

--- react-prop-drilling/src/App.jsx
+++ react-prop-drilling/src/App.jsx
@@ -1,16 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useContext, useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
+import { CountContext } from './context'
 
 function App() {
   const [count, setCount] = useState(0)
 
+  // wrap anyone that wants to use the teleported value inside a provider
+
   return (
-    <>
-      
-    </>
+    <div>
+        <CountContext.Provider value={count}>
+        <Count count ={count} setCount={setCount}/>
+        </CountContext.Provider>
+    </div>
   )
+}
+
+function Count({ setCount }){
+
+  return (<div>
+      <CountRenderer />
+      <Buttons  setCount={setCount} />
+  </div>
+  )
+}
+
+function CountRenderer(){
+    const count = useContext(CountContext)
+    return <div>
+      {count}
+    </div>
+}
+
+function Buttons({setCount}){
+  const count = useContext(CountContext)
+  return (
+    <div>
+      <button onClick={()=>{
+        setCount(count+1)
+      }}>Increase</button>
+      <button onClick={()=>{
+        setCount(count-1)
+      }}>Dcrease</button>
+    </div>
+  )
+
 }
 
 export default App
