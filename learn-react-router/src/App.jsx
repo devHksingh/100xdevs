23ab200d9d6c0878b6a4dcc0c37acdb1d2ec3a21
devHksingh <hksingh.dev@gmail.com>
2024-01-17 22:56:49 +0530

    initial work  and ui  done

---
+++ learn-react-router/src/App.jsx
@@ -0,0 +1,22 @@
+// import { useState } from 'react'
+import Header from './components/Header/Header'
+import Home from './components/Home/Home'
+import Footer from './components/Footer/Footer'
+
+
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='text-center font-mono font-extrabold bg-green-600 rounded-lg p-4 text-4xl'>React Router</h1>
+      {/* <Header />
+      <Home />
+      <Footer /> */}
+    </>
+  )
+}
+
+export default App
