e94b976ac4fa47e0e4d2c17fb7c2ccf25260ae1a
devHksingh <hksingh.dev@gmail.com>
2024-02-11 19:47:33 +0530

    Build authentication service with appwrite

--- reduxToolkit-Todo/src/App.jsx
+++ reduxToolkit-Todo/src/App.jsx
@@ -1,19 +1,15 @@
-
-
-import './App.css'
-import AddTodo from './components/AddTodo'
-import Todos from './components/Todos'
+import "./App.css";
+import AddTodo from "./components/AddTodo";
+import Todos from "./components/Todos";
 
 function App() {
-  
-
   return (
     <>
       <h1>Learn redux toolkit</h1>
-      <AddTodo/>
-      <Todos/>
+      <AddTodo />
+      <Todos />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
