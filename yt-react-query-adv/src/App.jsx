51832c75161d8556d3435a60c1dc12ae945ff87c
devHksingh <hksingh.dev@gmail.com>
2024-04-04 17:54:19 +0530

    initail setup for adv react query

---
+++ yt-react-query-adv/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className='font-semibold text-center text-sky-600'>Adv react query</h1>
+    </>
+  )
+}
+
+export default App
