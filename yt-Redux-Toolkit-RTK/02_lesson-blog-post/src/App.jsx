fb662df15bb2327b83f711f7c693f84b5325f6ff
devHksingh <hksingh.dev@gmail.com>
2024-04-16 21:27:22 +0530

    add post and user functionality

--- yt-Redux-Toolkit-RTK/02_lesson-blog-post/src/App.jsx
+++ yt-Redux-Toolkit-RTK/02_lesson-blog-post/src/App.jsx
@@ -1,5 +1,6 @@
 // import { useState } from 'react'
 import './App.css'
+import AddPostForm from './features/posts/AddPostForm'
 import PostsList from './features/posts/PostsList'
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
 
   return (
     <>
+      <AddPostForm/>
       <PostsList/>
     </>
   )
--- yt-Redux-Toolkit-RTK/02_lesson-blog-post/src/features/posts/postsSlice.js
+++ yt-Redux-Toolkit-RTK/02_lesson-blog-post/src/features/posts/postsSlice.js
@@ -1,11 +1,11 @@
-import { createSlice } from "@reduxjs/toolkit";
-
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+import {sub } from 'date-fns'
 const initialState = [
   {
     id: "1",
     title: "Learning Redux Toolkit",
     content: "I've heard good things.",
-    // date: sub(new Date(), { minutes: 10 }).toISOString(),
+    date: sub(new Date(), { minutes: 10 }).toISOString(),
     // reactions: {
     //     thumbsUp: 0,
     //     wow: 0,
@@ -18,7 +18,7 @@ const initialState = [
     id: "2",
     title: "Slices...",
     content: "The more I say slice, the more I want pizza.",
-    // date: sub(new Date(), { minutes: 5 }).toISOString(),
+    date: sub(new Date(), { minutes: 5 }).toISOString(),
     // reactions: {
     //     thumbsUp: 0,
     //     wow: 0,
@@ -33,8 +33,22 @@ export const postsSlice = createSlice({
   name: "posts",
   initialState,
   reducers: {
-    postAdded(state, action) {
-      state.push(action.payload);
+    postAdded: {
+      reducer(state, action) {
+        state.push(action.payload);
+      },
+      //  prepare callback
+      prepare(title, content,userId) {
+        return {
+          payload: {
+            id: nanoid(),
+            title,
+            content,
+            date : new Date().toISOString(),
+            userId,
+          },
+        };
+      },
     },
   },
 });
