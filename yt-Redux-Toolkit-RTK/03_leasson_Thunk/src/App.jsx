04d1b0d3ca570e2481d399ea82d84ee85d2098ad
devHksingh <hksingh.dev@gmail.com>
2024-04-19 13:52:52 +0530

    add async thunk

--- yt-Redux-Toolkit-RTK/03_leasson_Thunk/src/App.jsx
+++ yt-Redux-Toolkit-RTK/03_leasson_Thunk/src/App.jsx
@@ -1,14 +1,19 @@
-import PostsList from "./features/posts/PostsList";
-import AddPostForm from "./features/posts/AddPostForm";
+// import PostsList from "./features/posts/PostsList";
+// import AddPostForm from "./features/posts/AddPostForm";
 import './App.css'
+import AddPostForm from './features/posts/AddPostForm'
+import PostsList from './features/posts/PostsList'
 
 function App() {
   
 
   return (
     <>
-      <AddPostForm />
-      <PostsList />
+      {/* <AddPostForm />
+      <PostsList /> */}
+
+      <AddPostForm/>
+      <PostsList/>
     </>
   )
 }
