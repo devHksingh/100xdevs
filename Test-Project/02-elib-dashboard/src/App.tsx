b0b3015b4c814be1c01f130f47c447e81cdcd5ae
devHksingh <hksingh.dev@gmail.com>
2024-04-26 16:30:00 +0530

    add setup for shadcn ui

--- Test-Project/02-elib-dashboard/src/App.tsx
+++ Test-Project/02-elib-dashboard/src/App.tsx
@@ -1,12 +1,14 @@
 
 import './App.css'
+import { Button } from '@/components/ui/button'
 
 function App() {
   
 
   return (
     <>
-      <h1>wlecome to elib dashbord</h1>
+      <h1 className='text-orange-400'>wlecome to elib dashbord</h1>
+      <Button variant={'outline'}>click me!</Button>
     </>
   )
 }
