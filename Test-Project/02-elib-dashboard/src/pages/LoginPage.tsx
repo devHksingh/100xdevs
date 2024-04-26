f7347e03ce3f0d73acb4225acd02c7bf46a36460
devHksingh <hksingh.dev@gmail.com>
2024-04-26 23:40:16 +0530

    fix link

--- Test-Project/02-elib-dashboard/src/pages/LoginPage.tsx
+++ Test-Project/02-elib-dashboard/src/pages/LoginPage.tsx
@@ -2,11 +2,23 @@ import { Button } from "@/components/ui/button"
 import { Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
+import { useRef } from "react"
 import { Link } from "react-router-dom"
 
 
 
 const LoginPage = () => {
+  const emailRef = useRef<HTMLInputElement>(null)
+  const passwordRef = useRef<HTMLInputElement>(null)
+  const handleLoginSubmit =()=>{
+    const email = emailRef.current?.value
+    const password = passwordRef.current?.value
+
+    // make server call
+    console.log('data',password,email);
+    
+    
+  }
   return (
     <section className="flex items-center justify-center h-screen">
             <Card className="w-full max-w-sm">
@@ -19,18 +31,18 @@ const LoginPage = () => {
         <CardContent className="grid gap-4">
             <div className="grid gap-2">
             <Label htmlFor="email">Email</Label>
-            <Input id="email" type="email" placeholder="m@example.com" required />
+            <Input ref={emailRef} id="email" type="email" placeholder="m@example.com" required />
             </div>
             <div className="grid gap-2">
             <Label htmlFor="password">Password</Label>
-            <Input id="password" type="password" required />
+            <Input ref={passwordRef} id="password" type="password" required />
             </div>
         </CardContent>
         <CardFooter className="flex flex-col">
-            <Button className="w-full">Sign in</Button>
+            <Button onClick={handleLoginSubmit} className="w-full">Sign in</Button>
             <div className="mt-4 text-sm text-center">
               Don't have an account?{" "}
-              <Link to={'auth/login/register'} className="underline">
+              <Link to={'/auth/register'} className="underline">
                 Sign up
               </Link>
             </div>
