3ef6fae2bfa03a84d3884eec38f1ed35d1bdb9b2
devHksingh <hksingh.dev@gmail.com>
2024-05-30 15:29:46 +0530

    fix minnor bugg

--- framerMotion-ui/01-staggered-grid/src/components/NavBar.tsx
+++ framerMotion-ui/01-staggered-grid/src/components/NavBar.tsx
@@ -1,69 +1,73 @@
-import { useRef, useState } from "react"
-import { motion } from "framer-motion"
+import React, { useRef, useState } from "react";
+import { motion } from "framer-motion";
 
-export const SlideTabsExample = ()=>{
-    return (
-        <div className="grid h-screen place-content-center bg-neutral-200">
-            <SlideTabs/>
-        </div>
-    )
-}
+export const SlideTabsExample = () => {
+  return (
+    <div className="py-20 bg-neutral-100">
+      <SlideTabs />
+    </div>
+  );
+};
 
+const SlideTabs = () => {
+  const [position, setPosition] = useState({
+    left: 0,
+    width: 0,
+    opacity: 0,
+  });
 
-const SlideTabs = ()=>{
-    const [position,setPosition] =useState({
-        left:0,
-        width:0,
-        opacity:0
-    })
-    return (
-        <ul
-        onMouseLeave={()=>{
-            setPosition((previousValue)=>({
-                ...previousValue,
-                opacity:0
-            }))
-        }}
-        className="relative flex p-1 mx-auto bg-white border-black rounded-full w-fit boder-2"
-        >
-            <Tab setPosition={setPosition}>Home</Tab>
-            <Tab setPosition={setPosition}>Pricing</Tab>
-            <Tab setPosition={setPosition}>Features</Tab>
-            <Tab setPosition={setPosition}>Docs</Tab>
-            <Tab setPosition={setPosition}>Blog</Tab>
-            <Coursor position ={position}/>
-        </ul>
-    )
-}
+  return (
+    <ul
+      onMouseLeave={() => {
+        setPosition((pv) => ({
+          ...pv,
+          opacity: 0,
+        }));
+      }}
+      className="relative flex p-1 mx-auto bg-white border-2 border-black rounded-full w-fit"
+    >
+      <Tab setPosition={setPosition}>Home</Tab>
+      <Tab setPosition={setPosition}>Pricing</Tab>
+      <Tab setPosition={setPosition}>Features</Tab>
+      <Tab setPosition={setPosition}>Docs</Tab>
+      <Tab setPosition={setPosition}>Blog</Tab>
 
-const Tab = ({children,setPosition})=>{
-    const ref = useRef(null)
-    return(
-        <li
-        ref={ref}
-        onMouseEnter={()=>{
-            if(!ref.current) return;
-            
-            // width
-            const {width} = ref.current.getBoundingClientRect()
-            console.log(width);
-            setPosition({
-                width,
-                opacity:1,
-                left:ref.current.offsetLeft
-            })
-            
-        }}
-        className="relative z-10 block px-3 cursor-pointer py-1.5 text-xs uppercase text-white mix-blend-difference md:px-5 md:py-3 md:text-base"
-        >
-           {children} 
-        </li>
-    )
-}
+      <Cursor position={position} />
+    </ul>
+  );
+};
 
-const Coursor =({position})=>{
-    return <motion.li
-        animate={position}
-        className="absolute z-0 bg-black rounded-full h-7 md:h-12"
+const Tab = ({ children, setPosition }) => {
+  const ref = useRef(null);
+
+  return (
+    <li
+      ref={ref}
+      onMouseEnter={() => {
+        if (!ref?.current) return;
+
+        const { width } = ref.current.getBoundingClientRect();
+
+        setPosition({
+          left: ref.current.offsetLeft,
+          width,
+          opacity: 1,
+        });
+      }}
+      className="relative z-10 block cursor-pointer px-3 py-1.5 text-xs uppercase text-white mix-blend-difference md:px-5 md:py-3 md:text-base"
+    >
+      {children}
+    </li>
+  );
+};
+
+const Cursor = ({ position }) => {
+  return (
+    <motion.li
+      animate={{
+        ...position,
+      }}
+      className="absolute z-0 bg-black rounded-full h-7 md:h-12"
     />
-}
+  );
+};
