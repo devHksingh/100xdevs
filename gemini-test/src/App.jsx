8c5877e7c97138be74e2e82aa94884466218977d
devHksingh <hksingh.dev@gmail.com>
2024-07-02 15:39:13 +0530

    setup

---
+++ gemini-test/src/App.jsx
@@ -0,0 +1,16 @@
+import Text_genration from "./components/text_text_genration/Text_genration"
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+        <Text_genration/>
+      </div>
+    </>
+  )
+}
+
+export default App
