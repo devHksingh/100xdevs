3a59a8d90d881a99579471095f48bd4b96a8abbc
devHksingh <hksingh.dev@gmail.com>
2024-07-18 18:50:04 +0530

    useNavigateHook

--- react-router-dom-v6/src/pages/NotFound.jsx
+++ react-router-dom-v6/src/pages/NotFound.jsx
@@ -1,6 +1,13 @@
-import {Navigate} from "react-router-dom"
+import { useEffect } from "react"
+import {Navigate,useNavigate} from "react-router-dom"
 
 const NotFound = () => {
+  const navigate = useNavigate()
+  useEffect(()=>{
+    setTimeout(()=>{
+      navigate("/")
+    },2000)
+  },[navigate])
   return (
     <Navigate to={'/'} />
   )
